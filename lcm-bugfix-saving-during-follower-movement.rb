403209fe828c512d9f0fb2636305e2a1274a69a4
Max <101079236+MaxwellWellman@users.noreply.github.com>
2024-06-29 21:05:13 +0300

    Update lcm-bugfix-saving-during-follower-movement.rb

--- lcm-bugfix-saving-during-follower-movement.rb
+++ lcm-bugfix-saving-during-follower-movement.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 %w[██████████████████████████████████████████████████████████████████████████]
 '  █ LCM BUGFIX █████████████████████████████████████████████████████████████'
 #  █ Saving During Follower Movement ████████████████████████████████████████
@@ -11,65 +10,32 @@
 '  ████████████████████████████████████████████████████████ MAXWELL WELLMAN █'
 #  ██████████████████████████████████████████████████████████████████████████
 
-=begin
-
- Fixes the bug where saving the game during character movement will delete
- the savefile you're trying to overwrite.
-
-=end
-
-module MW_Core
-  if ($mw_core_version ||= 0) <= 0
-
-    Class.module_eval do
-
-      def patch(method_name, &new_body)
-        old_body = instance_method(method_name)
-        class_exec do
-          define_method method_name do |*args, &block|
-            new_body.call_with_context(
-              self,
-              old_body.bind(self),
-              *args,
-              &block
-            )
-          end
-        end
-      end
-
-    end
-
-    Proc.module_eval do
-
-      def call_with_context(caller, *args, &block)
-        temp_name = "___#{rand}___"
-        caller.class.send(:define_method, temp_name, &self)
-        method = caller.class.send(:instance_method, temp_name)
-        caller.class.send(:undef_method, temp_name)
-        method.bind(caller)[*args, &block]
-      end
-
-    end
-
-    Object.module_eval do
-
-      def invar(name)
-        instance_variable_get(name)
-      end
-
-      def invar!(name, val)
-        instance_variable_set(name, val)
-      end
-
-    end
-
+proc do
+  unless ($imported ||= {})['Liam-LisaCoreMove']
+    raise("LCM BUGFIX - 'Saving During Follower Movement' must be ordered after LCM")
   end
-end
 
-DataManager.singleton_class.class_eval do
+  p = Object.method_defined?(:patch)
 
-  if ($imported ||= {})['Liam-LisaCoreMove']
-    patch(:save_game) do |super_, index|
+  def patch(method_name, &new_body)
+    old_body = instance_method(method_name)
+    class_exec do
+      define_method method_name do |*args, &block|
+        temp_name = "unbound"
+        while singleton_class.method_defined?(temp_name)
+          temp_name.concat("_")
+        end
+        define_singleton_method(temp_name, &new_body)
+        unbound = singleton_class.instance_method(temp_name)
+        singleton_class.send(:remove_method, temp_name)
+        unbound.bind(self)[old_body.bind(self), *args, &block]
+      end
+    end
+  end
+
+  DataManager.singleton_class.class_eval do
+
+    patch(:save_game) do |_, index|
       fibers = {}
       [$game_player, $game_player.followers].flatten.each do |flw|
         next if flw.lcmCustomConsecutiveMoveRoutesFiber.nil?
@@ -79,7 +45,11 @@ DataManager.singleton_class.class_eval do
         flw.lcmCustomConsecutiveMoveRoutesFiber = nil
       end
 
-      save_game_without_rescue(index)
+      begin
+        save_game_without_rescue(index)
+      rescue => err
+        raise(err)
+      end
 
       [$game_player, $game_player.followers].flatten.each do |flw|
         next if flw.actor.nil?
@@ -88,26 +58,23 @@ DataManager.singleton_class.class_eval do
       end
       true
     end
-  else
-    msg = "\n\n\n\n\n\n\n"
-    msg.concat 'LCM BUGFIX : Saving During Follower Movement'
-    msg.concat "\nmust be ordered after LCM."
-    msg.concat "\n\n\n\n\n\n\n"
-    raise msg
+
   end
 
-end
+  Scene_Map.class_eval do
 
-Scene_Map.class_eval do
-
-  patch(:start) do |super_|
-    super_[]
-    [$game_player, $game_player.followers].flatten.each do |flw|
-      flw.lcmCustomConsecutiveMoveRoutesList = nil
-      flw.invar!(:@move_route, nil)
-      flw.lcmCustomConsecutiveMoveRoutesFiber = nil
-      flw.setuplcmCustomConsecutiveMoveRoutes(RPG::MoveRoute.new)
+    patch(:start) do |super_|
+      super_[]
+      [$game_player, $game_player.followers].flatten.each do |flw|
+        flw.lcmCustomConsecutiveMoveRoutesList = nil
+        flw.instance_variable_set(:@move_route, nil)
+        flw.lcmCustomConsecutiveMoveRoutesFiber = nil
+        flw.setuplcmCustomConsecutiveMoveRoutes(RPG::MoveRoute.new)
+      end
     end
+
   end
 
-end
+  Object.remove_method(:patch) if p
+
+end[]
